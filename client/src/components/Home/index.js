a27593b756e448a6486b46991b73731951f0624c
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-10-07 15:18:25 +0300

    call button in home component

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react"
+import Button from "../sharedComponent/Button"
 
 const Home = () => {
   return (
     <div>
       <p>in home</p>
+      <Button color="white"> Hello </Button>
     </div>
   )
 }
