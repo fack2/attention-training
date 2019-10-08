864e01ee4fcc7fb8f9114c4a85dcfd8f022da300
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-10-08 16:36:14 +0300

    create  Landing page component

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import StyledLink from '../sharedComponent/Button'
+import React from "react"
+import StyledLink from "../sharedComponent/Button"
+import homeBackgroud from "../../assets/homeBackground.png"
+import logo from "../../assets/logoWeb.png"
+import {
+  StyledImg,
+  TextStyled,
+  StyledLogo,
+  BackgroundDiv
+} from "./HomeStyledComponent"
 
 const Home = () => {
   return (
-    <div>
-      <p>in home</p>
-      <StyledLink margin="5em" width="60px" to="/ss">
-        Hello
+    <>
+      <BackgroundDiv>
+        <StyledImg alt="home" src={homeBackgroud} />
+        <StyledLogo alt="logo" src={logo} />
+      </BackgroundDiv>
+
+      <TextStyled>
+        The key to thriving is knowledge. Learn to make use of your ADHD
+        superpowers
+      </TextStyled>
+      <StyledLink margin="5em" width="90px " fontSize="20px" to="/ss">
+        Let's Go
       </StyledLink>
-    </div>
+    </>
   )
 }
 export default Home
