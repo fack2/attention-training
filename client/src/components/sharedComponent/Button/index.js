dbd973d413d1538c253c2fee0453b1f019b30877
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-10-08 10:59:13 +0300

    edit pading and edit names to camelCase

--- client/src/components/sharedComponent/Button/index.js
+++ client/src/components/sharedComponent/Button/index.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 const StyledLink = styled(Link)`
   background: ${props => (props.primary ? '#ED6237' : '#378BED')};
-  font-size: ${({ font_size }) => font_size || '16px'};
+  font-size: ${({ fontSize }) => fontSize || '16px'};
   
   color: ${({ color }) => color || 'white'};
   margin: ${({ margin }) => margin || '1em'};
   box-shadow: 0px 10px 25px rgba(84, 104, 255, 0.3);
-  border-radius: ${({ border_radius }) => border_radius || '15px'};
-  padding:5%;
+  border-radius: ${({ borderRadius }) => borderRadius || '15px'};
+  padding:${({ borderRadius }) => borderRadius || '5%'};
   display: inline-block;
   text-align: center;
   width:${({ width }) => width || '40px'}
