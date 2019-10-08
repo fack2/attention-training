c3b2fc323b7751eb93562ed93511c737ec93c018
YousefQwasmeh <151037@ppu.edu.ps>
2019-10-08 14:30:17 +0300

    abstract backbutton logic #20 separate antd logic from wrapper component more destructuring

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -2,10 +2,10 @@ import React from "react"
 import BackButton from "../sharedComponent/BackButton"
 import StyledLink from "../sharedComponent/Button"
 
-const Home = props => {
+const Home = ({ history }) => {
   return (
     <div>
-      <BackButton props={props}></BackButton>
+      <BackButton history={history}></BackButton>
       <p>in home</p>
       <StyledLink margin="5em" width="60px" to="/ss">
         Hello
--- client/src/components/sharedComponent/BackButton/index.js
+++ client/src/components/sharedComponent/BackButton/index.js
@@ -1,22 +1,14 @@
 import React from "react"
-import { Icon } from "antd"
-import "antd/dist/antd.css"
-import styled from "styled-components"
-const BackButton = ({ props }) => {
-  const { history } = props
-  const BackIcon = styled(Icon)`
-    color: ${({ color }) => color || "#70b1fa"};
-    margin: ${({ margin }) => margin || "1%"};
-    float: left;
-  `
+import BackIcon from "./BackIcon"
 
+const BackButton = ({ history }) => {
   return (
     <BackIcon
       type="arrow-left"
       onClick={() => {
         history.goBack()
       }}
-    ></BackIcon>
+    />
   )
 }
 export default BackButton
