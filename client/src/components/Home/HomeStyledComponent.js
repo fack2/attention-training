6d0ebabf94c9ca1a43ab70015a2542b3f339ffd3
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-10-08 16:35:14 +0300

    create Home styled component

---
+++ client/src/components/Home/HomeStyledComponent.js
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+
+const BackgroundDiv = styled.div`
+  position: relative;
+`
+const StyledImg = styled.img`
+  background: no-repeat top left;
+  background-size: cover;
+  clear: both;
+  padding: 0px;
+  margin: 0px;
+  z-index: 2;
+  clip-path: ellipse(564px 200px at -34% 24%);
+}
+`
+
+const StyledLogo = styled.img`
+  z-index: 1;
+  position: absolute;
+  margin-left: -273px;
+  margin-top: 94px;
+`
+
+const TextStyled = styled.p`
+  text-align: center;
+  font-size: 25px;
+`
+
+export { StyledImg, TextStyled, StyledLogo, BackgroundDiv }
