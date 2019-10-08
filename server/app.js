849437c07c309a21378a16f6a2aab6e2a4244a26
Sahar Adem <sahar.adem95@gmail.com>
2019-10-08 09:44:39 +0300

    edit the way of setting the port

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.set('port', process.env.PORT || 4000)
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
 })
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
-const PORT = process.env.PORT || 4000
 const app = require('./app')
 
-app.listen(PORT, () => {
-  console.log(`App running on http://localhost:${PORT}`)
+app.listen(app.get('port'), () => {
+  console.log(`App running on http://localhost:${app.get('port')}`)
 })
