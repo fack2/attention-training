a7fb3d036459b325176eef0a51ce98e5dabd1953
YousefQwasmeh <151037@ppu.edu.ps>
2019-10-10 11:12:00 +0300

    seprate styles and jsx #20 update styles

--- client/src/components/sharedComponent/BackButton/BackIcon.js
+++ client/src/components/sharedComponent/BackButton/BackIcon.js
@@ -4,7 +4,6 @@ import styled from "styled-components"
 const BackIcon = styled(Icon)`
   color: ${({ color }) => color || "#70b1fa"};
   margin: ${({ margin }) => margin || "1%"};
-  // float: left;
   justify-content: flex-start;
 `
 export default BackIcon
