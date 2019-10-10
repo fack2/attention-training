a5f2ba0c17bd2cce6e01bf2f3d6ab85a11254766
Sahar Adem <sahar.adem95@gmail.com>
2019-10-10 10:11:31 +0300

    #20 editing style

--- client/src/components/sharedComponent/ProgressBar/index.js
+++ client/src/components/sharedComponent/ProgressBar/index.js
@@ -15,45 +15,32 @@ class ProgressBar extends Component {
       percent = 100;
       counter = 17
     }
-
     this.setState({ percent: percent + 100 / 18 })
     this.setState({ counter: counter + 1 })
-
   };
 
   render() {
     return (
-
       <ProgressBarContainer>
         <Counter>
           {this.state.counter} of 18
           </Counter>
-
         <ProgressBarStyle
           strokeColor={{
             '0%': '#108ee9',
             '100%': '#87d068',
           }}
-
           Color={'red'}
           percent={this.state.percent}
           showInfo={false}
         />
-
-
         <Button onClick={this.increase} >next </Button>
-
-
       </ProgressBarContainer>
-
-
     )
   }
 }
-
-
-
 export default ProgressBar
+
 
 
 
--- client/src/components/sharedComponent/ProgressBar/style.ProgressBar.js
+++ client/src/components/sharedComponent/ProgressBar/style.ProgressBar.js
@@ -2,27 +2,30 @@ import styled from "styled-components"
 import { Progress } from "antd"
 
 const ProgressBarContainer = styled.div`
- width: 500px ;
+ margin: auto;
+ width: 100% ;
+ max-width:270px ; 
+ min-width: 245px;
  display: flex;
  align-items: center;
  flex-direction: column;
  position: relative;
 `
-
 const Counter = styled.div`
  color:grey;
  text-align : center
 `
-
 const ProgressBarStyle = styled(Progress)`
- 
-border: grey 1px solid;
+  border: grey 1px solid;
   border-radius:6px;
-  width: 231px ;
+  width: 100% ;
+  max-width:260px ; 
+  min-width: 230px;
   margin:5px;
   .ant-progress-bg{
   border-radius:6px;
   height: 15px !important
+  background-image: linear-gradient(to right, rgb(112, 177, 250) 0%, rgb(112, 177, 250) 100%) !important
 }         
 `
 
