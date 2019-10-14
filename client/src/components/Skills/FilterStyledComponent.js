a6fea5f06684f8fb6ba96b148619b02fb096eecd
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-10-14 15:19:56 +0300

    edit filter styled component #8

--- client/src/components/Skills/FilterStyledComponent.js
+++ client/src/components/Skills/FilterStyledComponent.js
@@ -2,10 +2,11 @@ import styled from "styled-components"
 
 const DivStyled = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   margin-top: 5px;
   margin-bottom: 10px;
+  margin: auto;
 `
 const FilterStyled = styled.div`
   display: flex;
@@ -15,12 +16,12 @@ const FilterStyled = styled.div`
 `
 
 const FilterButton = styled.button`
-  background: ${({ background }) => background || "#c0c0c0"};
+  background: ${({ background }) => background || "white"};
   display: inline-block;
   box-shadow: 0px 10px 25px rgba(84, 104, 255, 0.3);
-  width: ${({ width }) => width || "92px"};
+  width: ${({ width }) => width || "111px"};
   height: ${({ height }) => height || "34px"};
-  border-radius: ${({ borderRadius }) => borderRadius || "5px"};
+  border-radius: ${({ borderRadius }) => borderRadius || "6px"};
   &:hover {
     background: ${({ hoverColor }) => hoverColor || "#696969"};
     color: ${({ hoverColor }) => hoverColor || "white"};
