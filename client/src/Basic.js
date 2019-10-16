c46430715cee8948ade8d9fbae24bc95d242cf67
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-10-16 20:25:23 +0300

    created a basic information component && styled copmonent #59

---
+++ client/src/Basic.js
@@ -0,0 +1,13 @@
+var parse = require("html-react-parser")
+const basicImg = "https://i.imgur.com/KRnTQ1v.png"
+
+const Basic = [
+  {
+    img: basicImg,
+    title: "ADHD Basic Information",
+    description: parse(
+      "<div><p>Attention Deficit Hyperactivity Disorder is characterized by time management challenges, hyper-focus, creativity and a strong difficulty in fitting within accepted norms of behaviour, in work and personal life.</p><p>People with ADHD have been shown to have different neurobiological pathway activation when undertaking tasks.</p><p>People with ADHD often require authenticity in relationships and work, to find meaning, and deploy their substantial creative skills.</p><p>ADHD is present in up to 10% of children and 5% of adults.</p><p>Most adults who have it are unawares of their condition and so attempt to live life by rules that don't work for them.</p><p>Leading to a 10x rate of depression and anxiety.</p><p>With the right management and information, people with ADHD, have substantial contributions to make in fields requiring exploratory behaviour and finding news paths in ambiguous environments.</p><p>Entrepreneurship, scientific research, performance arts, rescue services and sales are stimulating arenas within which people with ADHD thrive.</p></div>"
+    )
+  }
+]
+export default Basic
---
+++ client/src/components/BasicInformation/BasicInformationStyledComponent.js
@@ -0,0 +1,59 @@
+import styled from "styled-components"
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 800px;
+  list-style-type: none;
+
+  @media only screen and (min-width: 800px) {
+    margin: auto;
+  }
+`
+
+const Basicdiv = styled.div`
+  margin: 16px;
+  display: flex;
+  flex-direction: column;
+  list-style-type: none;
+`
+
+const Header = styled.h1`
+  font-size: 25px;
+  color: black;
+  text-align: center;
+`
+
+const TextDiv = styled.div`
+  width: 90%;
+  max-width: 332px;
+  min-width: 275px;
+  hyphens: auto;
+  margin: 0 auto;
+`
+
+const Text = styled.p`
+  font-size: 19px;
+  opacity: 0.8;
+`
+
+const Type = styled.p`
+  text-align: center;
+  font-size: 15px;
+  color: silver;
+`
+
+const BasicImg = styled.img`
+  align-items: center;
+`
+
+const List = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  @media only screen and (min-width: 800px) {
+    align-items: center;
+  }
+`
+
+export { PageWrapper, Basicdiv, Header, Text, TextDiv, Type, BasicImg, List }
