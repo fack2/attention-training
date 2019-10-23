b9924f9d188aca1201cbe95713d7732947a3fa9e
YousefQwasmeh <151037@ppu.edu.ps>
2019-10-23 10:10:42 +0300

    cal result #73 cal result in result-page

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -2,18 +2,22 @@ import React, { Component } from "react"
 import resultsImg from "../../assets/results.png"
 import resutlsData from "../resultsData"
 import Button from "../sharedComponent/Button"
-import {
-  Container,
-  Header,
-  TypeContainer,
-  Img,
-  Type,
-  Description
-} from "./style.Results"
+import { Container, Header, TypeContainer, Img, Type, Description } from "./style.Results"
 
 class Results extends Component {
-  render() {
-    const { id } = this.props.match.params
+  calID () {
+    return localStorage.getItem("inattentionScore") > 12 && localStorage.getItem("hyperactivityScore") > 12
+      ? 3 /* id=3 --- '/result/3' */
+      : localStorage.getItem("inattentionScore") > 12
+        ? 1 /* id=1 --- '/result/1' */
+        : localStorage.getItem("hyperactivityScore") > 12
+          ? 2 /* id=2 --- '/result/2' */
+          : 4 /* id=4 --- '/result/4' */
+  }
+
+  render () {
+    // const { id } = this.props.match.params
+    const id = this.calID()
     return (
       <Container>
         <Header>Results</Header>
