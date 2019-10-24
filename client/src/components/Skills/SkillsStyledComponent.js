2f0a8a53a3fb9e0dd54fef0cda6fa7d58db42552
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-10-24 12:34:30 +0300

    edit imge size

--- client/src/components/Skills/SkillsStyledComponent.js
+++ client/src/components/Skills/SkillsStyledComponent.js
@@ -3,7 +3,12 @@ import styled from "styled-components"
 const SkillImg = styled.img`
   border-radius: ${({ borderRadius }) => borderRadius || "4px"};
   width: 100%;
+  max-height: 203px;
+  min-height: 125px;
   height: 125px;
+  @media only screen and (min-width: 600px) {
+    height: 205px;
+  }
 `
 
 export default SkillImg
